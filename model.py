862b1ad2a68b82f6b52ee7474c4dce2a8371b2b8
Guilhem Niot <guilhem.niot@gmail.com>
2020-04-17 12:25:33 +0200

    Fix dead parts of the code

--- model.py
+++ model.py
@@ -100,7 +100,6 @@ daysQuarantine = 14 # duration of the quarantine | durée de la quarantaine
 
 # Librairies
 import random
-import math as m
 import numpy as np
 
 # -> sliders
@@ -463,7 +462,6 @@ def step(graph):
 # Interactive model below (it takes about 10-15 sec to appear and to run a simulation)
 
 import matplotlib.pyplot as plt
-from matplotlib import style
 
 fig, (ax, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=[10,10])
 xs = []
@@ -525,6 +523,7 @@ def draw_viz():
     plt.tight_layout()
 
 def update_prob(app_utilisation, report_to_app, quarantine_when_notif):
+    global nbIndividuals
     global utilApp
     global pReport
     global pQNotif
@@ -556,7 +555,6 @@ def update_prob(app_utilisation, report_to_app, quarantine_when_notif):
     y_D = []
     y_MS = []
     y_MAS = []
-    y_MPS = []
     y_S = []
     y_G = []
     y_Q = []
